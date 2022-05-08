0a6446a8a930e8738ad51a1f009165ba46a393fd
Gonzalo Silva <gonzalosilva@Gonzalos-MBP.attlocal.net>
2022-05-08 18:51:47 -0400

    Fix postal code

--- config/routes.rb
+++ config/routes.rb
@@ -30,10 +30,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get 'stripe_tiers/get_tiers', to: 'stripe_tier_plans#get_tiers', as: 'get_tiers'
+      get 'postal_codes/validate', to: 'postal_codes#validate_postal_code', as: 'validate_postal_code'
       resources :products, only: [:index,:create,:show, :update]
       resources :categories, only: [:index,:create,:show, :update]
       resources :postal_codes, only: [:index, :create, :show, :update]
-      get 'postal_codes/validate', to: 'postal_codes#validate_postal_code', as: 'validate_postal_code'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_25_220108) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "product"
     t.bigint "product_id", null: false
     t.index ["product_id"], name: "index_images_on_product_id"
   end
