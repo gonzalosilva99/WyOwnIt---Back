ae75bb4b56bcd10862d41a2c3e3e11254cfb51f5
gonzalosilva <gonga.silva1999@gmail.com>
2022-05-21 16:15:16 -0300

    Fix no price

--- app/controllers/api/v1/stripe_tier_plans_controller.rb
+++ app/controllers/api/v1/stripe_tier_plans_controller.rb
@@ -27,41 +27,39 @@ module Api
                 new_price = params[:new_price]
                 if(!new_price)
                     render json: { error: "Error, new price needed" }
-                end
-
-                user = current_user
-                customer = Stripe::Customer.list({email: user.email})
-                if(customer)
-                    subscriptions = []
-                    if customer.data.count > 0 
-                        subscriptions = Stripe::Subscription.list({customer: customer.data[0].id, status: 'active'})
+                else
+                    user = current_user
+                    customer = Stripe::Customer.list({email: user.email})
+                    if(customer)
+                        subscriptions = []
+                        if customer.data.count > 0 
+                            subscriptions = Stripe::Subscription.list({customer: customer.data[0].id, status: 'active'})
+                        else 
+                            render json: { error: "Error, there is no logged customer" }
+                        end
+    
+                        if(subscriptions.count > 0)
+                            subscription = subscriptions.data[0]
+                            if subscription
+                                Stripe::Subscription.update(
+                                    subscription.id,
+                                    {'items[0][id]': subscription.items.data[0].id, 'items[0][price]': new_price, 'proration_behavior': 'create_prorations'},
+                                )
+                                render json: { message: "The subscription have been successfully upgraded." }
+                            else
+                                render json: { error: "Error with the subscription." }
+                            end  
+                        else
+                            render json: { error: "Error, the customer does not have any active subscription" }
+                        end
                     else 
                         render json: { error: "Error, there is no logged customer" }
                     end
-
-                    if(subscriptions.count > 0)
-                        subscription = subscriptions.data[0]
-                        if subscription
-                            Stripe::Subscription.update(
-                                subscription.id,
-                                {'items[0][id]': subscription.items.data[0].id, 'items[0][price]': new_price, 'proration_behavior': 'create_prorations'},
-                            )
-                            render json: { message: "The subscription have been successfully upgraded." }
-                        else
-                            render json: { error: "Error with the subscription." }
-                        end  
-                    else
-                        render json: { error: "Error, the customer does not have any active subscription" }
-                    end
-                    
-                else 
-                    render json: { error: "Error, there is no logged customer" }
-                end
+                end 
             end 
 
             def cancel_subscription 
                 user = current_user
-                binding.pry
                 customer = Stripe::Customer.list({email: user.email})
                 if(customer)
                     subscriptions = []
